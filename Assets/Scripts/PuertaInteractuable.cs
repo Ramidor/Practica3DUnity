e0ee1ef4e0be6f13fe2f1c93ea2bc0623e40dd20
Adrianma13 <adrianmachucamorilla@gmail.com>
2026-04-29 22:23:41 +0200

    MAPEAO

---
+++ Assets/Scripts/PuertaInteractuable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaInteractuable : MonoBehaviour
+{
+    [Header("Configuración de la Puerta")]
+    public float costePuerta = 750f; // Puedes cambiar esto en el Inspector para cada puerta
+    public bool estaAbierta = false; // Ahora es pública para que el jugador la lea
+    
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void AlternarPuerta()
+    {
+        // En los juegos de zombies por oleadas, las puertas normalmente 
+        // se abren y se quedan abiertas para siempre.
+        estaAbierta = true; 
+        
+        // Le pasamos el valor al parámetro del Animator
+        anim.SetBool("estaAbierta", estaAbierta);
+    }
+}
